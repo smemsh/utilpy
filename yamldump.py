3369826076e2362e18215811de87a10faa82719b
Scott Mcdermott <scott@smemsh.net>
2016-08-01 01:13:49 -0700

    new: change to pattern of symlink -> canonical.py
    if we name them .py, this will make it easier later to add a __init__.py
    to the directory and make it importable as library code, in addition to
    just a standalone executable
    
    so far this affects yamldump because that's the only one we committed so
    far :-)

---
+++ yamldump.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""
+# yamldump
+#   pretty prints yaml files as python objects
+#
+# args:
+#   - any number of yaml files to print
+#
+# todo:
+#   - optionally pre-pass through jinja2
+#
+# http://smemsh.net/src/utilsh/
+# http://spdx.org/licenses/GPL-2.0
+# scott@smemsh.net
+#
+"""
+
+import yaml
+import sys
+from pprint import pprint as pp
+
+
+def usage(invname):
+
+    print(
+        "{}: parse and dump a yaml file\n"
+        "takes filenames as arguments"
+        .format(invname)
+    )
+    quit()
+
+
+def main():
+
+    invname = sys.argv.pop(0)
+    args = sys.argv
+    if not len(args) >= 1:
+        usage(invname)
+
+    for filename in args:
+        try:
+            with open(filename, "r") as f:
+                contents = f.read()
+            yamlstr = yaml.load(contents)
+            pp(yamlstr)
+
+        except FileNotFoundError:
+            print("{}: {}: nsfod"
+                  .format(invname, filename))
+            quit()
+
+        except:
+            print("{}: unhandled exception".format(invname))
+            raise
+
+
+if __name__ == "__main__":
+    main()
