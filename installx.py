beb7eaae6097b54e076c9b7208663101c7b11fd0
Scott Mcdermott <scott@smemsh.net>
2024-10-28 16:08:28 -0700

    - headers: url: update source repo url to github, not smemsh.net
    no longer using our own repo for this

--- installx.py
+++ installx.py
@@ -12,7 +12,7 @@ installs in-dir exe files and symlinks, or all .rclinks, to [homedir]
     descendants of ${1:-.}/ and the end target is in ${1:-.}/
 
 """
-__url__     = 'http://smemsh.net/src/utilpy/'
+__url__     = 'https://github.com/smemsh/utilpy/'
 __author__  = 'Scott Mcdermott <scott@smemsh.net>'
 __license__ = 'GPL-2.0'
 
